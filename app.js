e0b00c45c038d4e4843672274a1f9998ff3b3fae
mohamedeissa448 <mohamedeissa448@gmail.com>
2020-04-22 13:22:12 +0200

    change database url to local

--- app.js
+++ app.js
@@ -23,12 +23,9 @@ app.set("view engine", "ejs");
 
 var mongoose = require("mongoose");
 mongoose //"mongodb://localhost:27017/opeco"
-  .connect(
-    "mongodb+srv://admin:admin@cluster0-lnmba.mongodb.net/test?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true
-    }
-  )
+  .connect("mongodb://localhost:27017/opeco", {
+    useNewUrlParser: true
+  })
   .then(() => {
     console.log("connected to DB");
   })
